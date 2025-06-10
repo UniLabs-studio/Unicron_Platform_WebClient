d5e90a6d9919c6bec5890921e6cbd30f9276e58d
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2025-06-10 19:52:49 +0300

    edit files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
